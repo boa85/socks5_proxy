076d93dc156fa2fb3e4075a4a37246825b1c7feb
boa <belaev.oa@gmail.com>
2017-09-05 10:15:39 +0600

    added namespace service

---
+++ service/include/service.h
@@ -0,0 +1,17 @@
+//
+// Created by boa on 05.09.17.
+//
+
+#ifndef SOCKS5_PROXY_SERVICE_H
+#define SOCKS5_PROXY_SERVICE_H
+
+#include <boost/system/system_error.hpp>
+
+namespace socks5_proxy {
+    namespace service {
+        namespace b_sys = boost::system;
+    }
+}
+
+
+#endif //SOCKS5_PROXY_SERVICE_H
---
+++ service/src/service.cpp
@@ -0,0 +1,11 @@
+//
+// Created by boa on 05.09.17.
+//
+
+#include "../include/service.h"
+
+namespace socks5_proxy {
+    namespace service {
+
+    }
+}
