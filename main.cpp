4562e09a1d26801ec7e603b16a35b45fc62e0920
boa <belaev.oa@gmail.com>
2017-09-05 13:07:07 +0600

    added session class implementation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
-
-
+#include "application/include/application.h"
+#include "server/include/server.h"
 
 int main(int argc, char * argv[]) {
+    socks5_proxy::application::Application app(argc, argv);
 
-    return 0;
+    if (int code = app.prepare() != 0) {
+        return 0 ? code == -1 : code;
+    }
+
+    socks5_proxy::server::Server server(app.ioLoop());
+
+    server.listenV4(app.getPort());
+    server.listenV6(app.getPort());
+
+    return app.exec();
 }
