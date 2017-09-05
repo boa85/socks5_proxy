93faa8029cbbd519cb941438cb5241c2ee12f814
boa <belaev.oa@gmail.com>
2017-09-05 11:22:19 +0600

    added Application class with parser command line arguments

--- service/include/service.h
+++ service/include/service.h
@@ -7,9 +7,31 @@
 
 #include <boost/system/system_error.hpp>
 
+
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/program_options.hpp>
+#include <boost/asio/signal_set.hpp>
+
 namespace socks5_proxy {
     namespace service {
         namespace b_sys = boost::system;
+        namespace po = boost::program_options;
+        namespace ph = std::placeholders;
+
+        using Chunk = std::array<uint8_t, 8192>;
+        using IOLoop = boost::asio::io_service;
+        using AddressV4 = boost::asio::ip::address_v4;
+        using AddressV6 = boost::asio::ip::address_v6;
+        using Acceptor = boost::asio::ip::tcp::acceptor;
+        using EndPoint = boost::asio::ip::tcp::endpoint;
+        using Socket = boost::asio::ip::tcp::socket;
+        using ErrorCode = boost::system::error_code;
+        using Options = boost::program_options::options_description;
+        using OptionMap = boost::program_options::variables_map;
+        using SignalHandler = boost::asio::signal_set;
+        using Address = boost::asio::ip::address;
+
     }
 }
 
